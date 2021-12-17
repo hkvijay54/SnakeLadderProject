6da6b56e8796a23c9a863137e4bfdefcdb88ba81
hkvijay54 <hkvijay54@gmail.com>
2021-12-18 02:49:53 +0530

    Snake Lader Program 3

--- SnakeLadderProject/Program.cs
+++ SnakeLadderProject/Program.cs
@@ -1,13 +1,38 @@
 ﻿using System;
-/// The Player rolls the die to get a number 
-/// between 1 to 6.
+/// The Player then checks for a Option.
+/// They are No Play,Ladder or Snake.
+
 
 class Program
 {
+    
     static void Main(string[] args)
     {
+        int Position = 0;
+        string[] choice = { "no play", "ladder", "Snake" };
         Random random = new Random();
         int dice = random.Next(1, 6);
-        Console.WriteLine("dice rolled = " + dice);
+        for(int i=0; i<=10; i++)
+        {
+            int randnum = random.Next(choice.Length);
+            if (choice[randnum] == "no play")
+            {
+                Console.WriteLine("No play for you, next round");
+                Console.WriteLine("------------------------------");
+                Position = Position + 0;
+            }
+            else if (choice[randnum] == "ladder")
+            {
+                Console.WriteLine("you got ladder "+ dice + "incremented");
+                Position = Position + dice;
+                Console.WriteLine(Position);
+            }
+            else
+            {
+                Console.WriteLine("you got snake " + dice + "decremented");
+                Position = Position - dice;
+                Console.WriteLine(Position);
+            }
+        }
     }
 }
