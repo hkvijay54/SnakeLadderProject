b506b75c68839fce758be0020a54374b4f752b14
hkvijay54 <hkvijay54@gmail.com>
2021-12-18 01:48:47 +0530

    snake ladder program 1

---
+++ SnakeLadderProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+/// snake ladder game played with single player 
+/// at start position 0
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int position = 0;
+        Console.WriteLine("starting the game at position " + position);
+    }
+}
