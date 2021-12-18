84c8123afe8498283e513bc72af2b78b327c53fc
hkvijay54 <hkvijay54@gmail.com>
2021-12-18 13:18:53 +0530

    Snake Ladder Program 6

--- SnakeLadderProject/Program.cs
+++ SnakeLadderProject/Program.cs
@@ -10,54 +10,33 @@ class Program
     {
         int Position = 0;
         int i = 0;
-        string[] choice = { "no play", "ladder", "Snake" };
+       
         Random random = new Random();
-        
-        while(i<=100)
+
+        while (i <= 100)
         {
             int dice = random.Next(1, 6);
-            int randnum = random.Next(choice.Length);
-            if (choice[randnum] == "no play")
+            Console.WriteLine("dice rolled and got = " + dice);
+            Console.WriteLine("Current you are in " + Position);
+            if (Position == 100)
             {
-                Console.WriteLine("No play for you, next round");
-                Console.WriteLine("------------------------------");
-                Position = Position + 0;
+                Console.WriteLine("Game Won" + Position);
+                break;
             }
-            else if (choice[randnum] == "ladder")
+            else if (Position + dice > 100)
             {
-                Console.WriteLine("you got ladder "+ dice + "incremented");
-               
-                if(Position == 100)
-                {
-                    Console.WriteLine("Payer won" + Position);
-                    break;
-                }
-                else if (Position+dice >= 100)
-                {
-                    Console.WriteLine("not possible to move" + dice);
-                    Position = Position + 0;
-                }
-                else
-                {
-                    Position = Position + dice;
-                    Console.WriteLine(Position);
-                }
+                Console.WriteLine("Not possible move");
+                Position = Position + 0;
+                i++;
             }
             else
             {
-                if (Position-dice <= 0)
-                {
-                    Console.WriteLine("Reached 0 . Restart the game");
-                    i = 0;
-                }
-                else
-                {
-                    Console.WriteLine("you got snake " + dice + "decremented");
-                    Position = Position - dice;
-                    Console.WriteLine(Position);
-                }
-
+                Position = Position + dice;
+                Console.WriteLine("The Position Now " + Position);
+                i++;
             }
+           
         }
+        Console.WriteLine("num " + i);
     }
 }
